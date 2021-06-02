a52d394517e5c681268f24ef6415ef4bfae8dc9a
atleastitsanethosman <79068217+atleastitsanethosman@users.noreply.github.com>
2021-06-01 22:10:48 -0400

    work on the generatemarkdownfunctions for badges

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,21 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  switch (license) {
+    case 'mit':
+      badge = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+    break;
+    case 'mozilla':
+      badge = "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)";
+    break;
+    case 'eclipse':
+      badge = "[![License](https://img.shields.io/badge/License-EPL%201.0-red.svg)](https://opensource.org/licenses/EPL-1.0)";
+    break;        
+    case 'none':
+      badge = "";
+    break;  
+  };
+};
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
