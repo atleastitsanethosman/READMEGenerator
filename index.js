9ce89807a5cf8b106438544bab02c6ec1c2e1ccf
atleastitsanethosman <79068217+atleastitsanethosman@users.noreply.github.com>
2021-06-01 21:37:43 -0400

    created inquirer prompts for data

--- index.js
+++ index.js
@@ -5,13 +5,96 @@ const generateMarkdown = require('./utils/generateMarkdown')
 
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'projectTitle',
+        message: 'What is the title of your project?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Enter a description of your project.',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Enter installation guidance or directions.',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Give details on how a user would use your project',
+    },
+    {
+        type: 'input',
+        name: 'authorName',
+        message: 'What is your name?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+    },
+    {
+        type: 'input',
+        name: 'gitId',
+        message: 'What is your GitHub username?',
+    },
+    {
+        type: 'input',
+        name: 'screenshot',
+        message: 'Enter the path to a screenshot of your project:',
+    },
+    {
+        type: 'input',
+        name: 'year',
+        message: 'Enter the copywrite year for your project:',
+    },
+    {
+        type: 'input',
+        name: 'citations',
+        message: 'Cite the resouces you used such as libraries, or API sites, etc.',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Select the license you want to use:',
+        choices: [
+        { 
+            name: 'MIT license',
+            value: 'mit'
+        },
+        {
+            name: 'Mozilla Public License 2.0',
+            value: 'mozilla'
+        },
+        {
+            name: 'Eclipse Public License version 2.0',
+            value: 'eclipse'
+        }]
+    },
+    {
+        type: 'input',
+        name: 'contribute',
+        message: 'Provide direction on outside contribution',
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Provide guidance on testing as part of the project.',
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((answers) => {
+        writeToFile('README.md',answers)
+    })
+}
 
 // Function call to initialize app
 init();
